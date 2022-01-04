abab19949257238b3fe7e5c5dd4d8d38bb59143e
Stephan <sgriesel@gmail.com>
2022-01-04 06:21:09 +0100

    start adding storybook

---
+++ src/stories/Banner.vue
@@ -0,0 +1,16 @@
+<template>
+<aside></aside>
+</template>
+
+<script>
+import './banner.css';
+// import { reactive, computed } from 'vue';
+
+export default {
+  name: 'my-banner',
+
+  props: {
+    variant: "info" | "congrats" | "documentation" | "danger",
+  },
+};
+</script>
